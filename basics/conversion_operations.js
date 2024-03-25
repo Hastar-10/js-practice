ee61bb7143e1df17af271873e665099239c1890a
Hastar-10 <100896215+Hastar-10@users.noreply.github.com>
2024-03-25 05:18:31 +0000

    operations done

--- basics/conversion_operations.js
+++ basics/conversion_operations.js
@@ -1,4 +1,4 @@
-let score = true
+/*let score = true
 
 console.log(typeof(score))
 console.log(typeof score)
@@ -21,4 +21,34 @@ console.log(booleanIsLoggedIn)
 let num=33
 let stringnumber=String(num)
 console.log(stringnumber)
-console.log(typeof stringnumber)
+console.log(typeof stringnumber)*/
+
+//*********** Operations **********//
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2**2)
+
+let str1="hello"
+let str2=" Saksham"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
